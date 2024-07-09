1f1d1d1e07f68e6239ad96a70a0f67e28b9d95f2
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-07-10 01:21:46 +0700

    6 and updated the box-shadow of 5

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 // defineProps<{
 //  msg: string
 //}>()
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12.00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="event-class"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -18,6 +35,6 @@
 }
 .event-card:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0,0 0, 0, 0.2);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
