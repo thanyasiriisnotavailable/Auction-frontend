d18884eefea9a0670a6f295ade94e49b7fec0ce3
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-07-22 18:13:32 +0700

    1.2

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get('/events')
+  },
+  getEvent(id: number) {
+    return apiClient.get('/events/' + id)
   }
 }
