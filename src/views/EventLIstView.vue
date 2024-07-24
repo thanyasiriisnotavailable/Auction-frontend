a400708e547102074e41fe998fc612ca4afec360
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-07-24 20:53:09 +0700

    2.7

--- src/views/EventLIstView.vue
+++ src/views/EventLIstView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import type { Event } from '@/types'
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
 
 const events = ref<Event[] | null>(null)
@@ -13,13 +13,16 @@ const props = defineProps({
 })
 const page = computed(() => props.page)
 onMounted(() => {
-  EventService.getEvents(2, page.value)
-    .then((response) => {
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.error('There was an error!', error)
-    })
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
+      .then((response) => {
+        events.value = response.data
+      })
+      .catch((error) => {
+        console.error('There was an error!', error)
+      })
+  })
 })
 </script>
 
