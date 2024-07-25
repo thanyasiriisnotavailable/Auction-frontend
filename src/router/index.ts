a687efb969000922ae0daed5c65f922b3dd7226b
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-07-25 23:47:29 +0700

    3.2

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import AboutView from '@/views/AboutView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,21 +17,29 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true
-    },
-    {
-      path: '/event/:id/register',
-      name: 'event-register-view',
-      component: EventRegisterView,
-      props: true
-    },
-    {
-      path: '/event/:id/edit',
-      name: 'event-edit-view',
-      component: EventEditView,
-      props: true
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
     },
 
     {
