b473f331e2dae5f0527bf7d39eff4ddd85cc9800
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-08-09 18:57:44 +0700

    7

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
     extend: {
       boxShadow: {
         'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
+      },
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       }
     },
   },
