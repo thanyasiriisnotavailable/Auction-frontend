0a0d8fe5c2d8594b60b67832326044db9a0877bd
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-08-09 18:41:55 +0700

    4.2 and fixed 4.1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0,2)',
+        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
       }
     },
   },
