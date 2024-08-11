db53a9ebb85567c071e6e001f2b584f471ec658d
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-08-11 19:06:18 +0700

    Fix the import of speed insights 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
