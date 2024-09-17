823168a17f99b0238b9cad0a147bc959705c8326
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-09-18 00:43:54 +0700

    update the baseURL by link to the backend lab
    http://localhost:8080

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/thanyasiriisnotavailable/331-intro-to-vite/',
+  baseURL: 'http://localhost:8080',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
