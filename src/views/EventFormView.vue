aff58bb8d5f5a39e9dc8f214ec5041b6485fa265
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-09-19 03:28:15 +0700

    7.4 create EventFormView.vue

---
+++ src/views/EventFormView.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import type { Event } from '@/types';
+import { ref } from 'vue';
+const event = ref<Event> ({
+    id: 0,
+    category: '',
+    title: '',
+    description: '',
+    location: '',
+    date: '',
+    time: '',
+    petsAllowed: false,
+    organizer: ''
+})
+</script>
+
+<template>
+    <div>
+        <h1>Create an event</h1>
+        <form>
+            <label>category</label>
+            <input v-model="event.category" type="text" placeholder="Category" class="field">
+            <h3>Name & describe your event</h3>
+            <label>Title</label>
+            <input v-model="event.title" type="text" placeholder="Title" class="field">
+            <label>Description</label>
+            <input v-model="event.description" type="text" placeholder="Description" class="field">
+            <h3>Where is your event?</h3>
+            <label>Location</label>
+            <input v-model="event.location" type="text" placeholder="Location" class="field">
+            <button class="button" type="submit">Submit</button>
+        </form>
+
+        <pre>{{ event }}</pre>
+    </div>
+</template>
+
+<!-- <script setup lang="ts">
+import type { EventItem } from '@/types';
+import { ref } from 'vue';
+const event = ref<EventItem> ({
+    id: 0,
+    category: '',
+    title: '',
+    description: '',
+    location: '',
+    date: '',
+    time: '',
+    petsAllowed: false,
+    organizer: ''
+})
+</script>
+
+<template>
+    <div>
+        <h1>Create an event</h1>
+        <form>
+            <label>category</label>
+            <input v-model="event.category" type="text" placeholder="Category" class="field">
+            <h3>Name & describe your event</h3>
+            <label>Title</label>
+            <input v-model="event.title" type="text" placeholder="Title" class="field">
+            <label>Description</label>
+            <input v-model="event.description" type="text" placeholder="Description" class="field">
+            <h3>Where is your event?</h3>
+            <label>Location</label>
+            <input v-model="event.location" type="text" placeholder="Location" class="field">
+            <button class="button" type="submit">Submit</button>
+        </form>
+
+        <pre>{{ event }}</pre>
+    </div>
+</template> -->
