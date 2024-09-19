bbba47f1053dbe434ffc3070744f90467724bceb
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-09-19 12:43:36 +0700

    7.10 update the DetailView and EventCard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,17 +3,6 @@ import type { Event } from '@/types'
 defineProps<{
   event: Event
 }>()
-// const event = ref({
-//  id: 5928101,
-//  category: 'animal welfare',
-//  title: 'Cat Adoption Day',
-//  description: 'Find your new feline friend at this event.',
-//  location: 'Meow Town',
-//  date: 'January 28, 2022',
-//  time: '12.00',
-//  petsAllowed: true,
-//  organizer: 'Kat Laydee'
-// })
 </script>
 
 <template>
@@ -21,7 +10,7 @@ defineProps<{
     <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
       <div class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp">
         <h2 class="text-xl">{{ event.title }}</h2>
-        <span>@{{ event.time }} on {{ event.date }}</span>
+        <span>{{ event.category }} @ {{ event.location }}</span>
       </div>
     </RouterLink>
   </div>
