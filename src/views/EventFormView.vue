47032b6fbe827458819f32ef89f9b877e87503ca
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-09-21 22:14:12 +0700

    4 mapping the Frontend  with Organizer

--- src/views/EventFormView.vue
+++ src/views/EventFormView.vue
@@ -14,7 +14,10 @@ const event = ref<Event>({
   date: '',
   time: '',
   petsAllowed: false,
-  organizer: ''
+  organizer: {
+    id: 0,
+    name: ''
+  }
 })
 
 const router = useRouter()
@@ -184,39 +187,3 @@ pre {
   word-wrap: break-word;
 }
 </style>
-
-<!-- <script setup lang="ts">
-import type { EventItem } from '@/types';
-import { ref } from 'vue';
-const event = ref<EventItem> ({
-    id: 0,
-    category: '',
-    title: '',
-    description: '',
-    location: '',
-    date: '',
-    time: '',
-    organizer: ''
-})
-</script>
-
-<template>
-    <div>
-        <h1>Create an event</h1>
-        <form>
-            <label>category</label>
-            <input v-model="event.category" type="text" placeholder="Category" class="field">
-            <h3>Name & describe your event</h3>
-            <label>Title</label>
-            <input v-model="event.title" type="text" placeholder="Title" class="field">
-            <label>Description</label>
-            <input v-model="event.description" type="text" placeholder="Description" class="field">
-            <h3>Where is your event?</h3>
-            <label>Location</label>
-            <input v-model="event.location" type="text" placeholder="Location" class="field">
-            <button class="button" type="submit">Submit</button>
-        </form>
-
-        <pre>{{ event }}</pre>
-    </div>
-</template> -->
